4c988482591a3de230f686365b3dd7bacf2b69b6
IlIllIIllIIIIlIllIllIIlIlIllIl <lukasrehak123@gmail.com>
2024-11-24 15:59:11 +0100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -804,7 +804,7 @@ do
 
 			local Logo = Instance.new("ImageLabel")
 			Logo.Name = "Logo"
-			Logo.Image = ""
+			Logo.Image = "http://www.roblox.com/asset/?id=133543161098719"
 			Logo.ScaleType = Enum.ScaleType.Fit
 			Logo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 			Logo.BackgroundTransparency = 1
